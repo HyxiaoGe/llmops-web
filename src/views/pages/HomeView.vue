69092957f4abe3e88c3a40db53b4785bcd8f4d01
HyxiaoGe <18889592303@163.com>
2025-01-01 21:03:17 +0800

    feat: add fetch config

---
+++ src/views/pages/HomeView.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+  <div>
+    <p>这是一个首页</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/space/apps/DetailView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { post } from '@/utils/request'
+
+const debug = async () => {
+  console.log('debug')
+  const resp = await post('/apps/550e8400-e29b-41d4-a716-446655440000/debug', {
+    body: { query: '你是谁' },
+  })
+  console.log(resp)
+}
+</script>
+
+<template>
+  <a-button @click="debug">发起1请求</a-button>
+</template>
+
+<style scoped></style>
